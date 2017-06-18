3b181d4ae5eda49ab34c7bde3d1df0998e0b0a3d
Arek Czarnik <arek.czarnik@rewe-digital.com>
2017-06-18 18:01:57 +0200

    first test so use the io monad to connect to disque and store test value, after that read it from monad transformer / either monad.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Scotty
 import Database.Disque
+import Data.ByteString (ByteString)
+import Control.Monad.IO.Class
+import qualified Data.ByteString as B
 
 main :: IO ()
-main = putStrLn "start Uthenga..."
+main = do
+    conn <- setup 
+    runDisque conn $ do
+       result <- addjob "test_queue" "test data" 0
+       liftIO $ print (disqueResult result)
+
+
+setup = do
+  conn <- connect $ disqueConnectInfo { connectHost = "127.0.0.1" }
+  return conn
+
+disqueResult :: Either Reply ByteString -> ByteString
+disqueResult result = do
+              case result of
+                Left  val ->  "error"
+                Right val ->  val
+

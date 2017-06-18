534790dbc4b2034742c247acc726964ab3e424f4
Arek Czarnik <arek.czarnik@rewe-digital.com>
2017-06-18 23:50:19 +0200

    add Data.String.Conversions convert text,strings,bytestrings

--- app/Main.hs
+++ app/Main.hs
@@ -5,29 +5,29 @@ module Main where
 import Web.Scotty
 import Database.Disque
 import Control.Monad.IO.Class
-import Data.Text.Encoding
 import qualified Data.ByteString as B
-import qualified Data.Text.Lazy.Encoding as TL
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Encoding as T
+import Data.String.Conversions (cs)
+
 
 main :: IO ()
 main = do
   conn <- setup
-  scotty 3000 $ get "/:queue/:text/:expire" (postJob conn)
+  scotty 3000 $ get "/:queue/:value/:expire" (postJob conn)
 
 postJob :: Connection -> ActionM ()
 postJob conn = do
-  createdJob <- liftIO (runDisque conn $ addjob "test_queue" "test data" 0)
+  queue <- param ("queue" :: TL.Text) :: ActionM TL.Text
+  value <- param ("value" :: TL.Text) :: ActionM TL.Text
+  expire <- param ("text" :: TL.Text) :: ActionM TL.Text
+  createdJob <- liftIO (runDisque conn $ addjob (cs queue) (cs value) 0)
   case createdJob of
-    Left val -> text (decode $ deconsReplay val)
-    Right val -> text (decode val)
+    Left val -> text (cs $ deconsReplay val)
+    Right val -> text (cs val)
 
 setup :: IO Connection
 setup = connect $ disqueConnectInfo {connectHost = "127.0.0.1"}
-
-decode :: B.ByteString -> TL.Text
-decode = TL.fromStrict . T.decodeUtf8
 
 deconsReplay :: Reply -> B.ByteString
 deconsReplay replay =

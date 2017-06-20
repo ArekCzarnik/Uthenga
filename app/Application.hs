0efa34819165ffe9290a7314e1e42cef45fcf33b
Arek Czarnik <arek.czarnik@rewe-digital.com>
2017-06-20 23:07:06 +0200

    add tests

--- app/Application.hs
+++ app/Application.hs
@@ -14,9 +14,10 @@ import Web.Scotty
 
 app :: Connection -> IO Application
 app disqueConnection = scottyApp $ do
-          let task = Task 1 "" "" 0
+          let task = Task 1 "1" "1" 0
           let subscriber = Subscriber 1 "" "" ""
           middleware logStdout -- log all requests; for production use logStdout
           get "/subscribers" (text $ cs $ encode subscriber)
-          get "/tasks" (text $ cs $ encode task)
+          get "/tasks" $ do
+              json task
           post "/tasks/:queue/:value/:expire" (addTask disqueConnection)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
 
 import Application (app)
 import Database.Disque
 import Test.Hspec
 import Test.Hspec.Wai
+import Test.Hspec.Wai.JSON
+import Data.Aeson (Value(..), object, (.=))
+
 
 main :: IO ()
 main = do
@@ -12,7 +15,7 @@ main = do
     with (app disqueConnection) $ do
       describe "GET /tasks" $ do
         it "responds with 200" $ do
-          get "/tasks" `shouldRespondWith` 200
+          get "/tasks" `shouldRespondWith` [json|{expire:0,value:"1",code:"1",id: 1}|] {matchStatus = 200, matchHeaders = ["Content-Type" <:> "application/json; charset=utf-8"]}
 
       describe "GET /subscribers" $ do
         it "responds with 200" $ do

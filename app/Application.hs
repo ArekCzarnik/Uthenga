ca82b538e65e6af151da65c4a20bc3cc3588eec3
Arek Czarnik <arek.czarnik@rewe-digital.com>
2017-06-23 10:15:07 +0200

    More Haskell like...Thanks Mischa.

--- app/Application.hs
+++ app/Application.hs
@@ -2,16 +2,16 @@
 
 module Application where
 
-import Data.Aeson (encode)
-import Data.String.Conversions (cs)
-import Database.Disque
-import qualified Database.RethinkDB as RethinkDB
-import Handlers.Task
-import Network.Wai (Application)
-import Network.Wai.Middleware.RequestLogger (logStdout)
-import Types.Subscriber
-import Types.Task
-import Web.Scotty
+import           Data.Aeson                           (encode)
+import           Data.String.Conversions              (cs)
+import           Database.Disque
+import qualified Database.RethinkDB                   as RethinkDB
+import           Handlers.Task
+import           Network.Wai                          (Application)
+import           Network.Wai.Middleware.RequestLogger (logStdout)
+import           Types.Subscriber
+import           Types.Task
+import           Web.Scotty
 
 app :: Maybe Connection -> Maybe RethinkDB.RethinkDBHandle -> IO Application
 app disqueConnection rethinkConnection =
@@ -19,12 +19,10 @@ app disqueConnection rethinkConnection =
     let task = Task 1 "1" "1" 0
     let subscriber = Subscriber 1 "" "" ""
     middleware logStdout -- log all requests; for production use logStdout
-    get "/subscribers" (text $ cs $ encode subscriber)
-    get "/tasks" $ json task
+    get "/subscribers" . text . cs . encode $ subscriber
+    get "/tasks" . json $ task
     post "/tasks/:queue/:value/:expire" (shouldAddTask disqueConnection)
 
 shouldAddTask :: Maybe Connection -> ActionM ()
-shouldAddTask maybeConn =
-    case maybeConn of
-            Just connection -> addTask connection
-            Nothing -> text "disque current offline!"
+shouldAddTask (Just connection) = addTask connection
+shouldAddTask Nothing           = text "disque current offline!"

b66435301c3be6c7ae2a99d47305276e566d184e
Arek Czarnik <arekczarnik@gmail.com>
2017-06-26 22:41:46 +0200

    add post to subscribers

--- src/Application.hs
+++ src/Application.hs
@@ -15,6 +15,7 @@ import           Web.Scotty
 import           Web.Scotty.Internal.Types (ActionT)
 import           Network.HTTP.Types.Status
 import           Data.Text.Lazy (Text)
+import           Database.Subscriber
 
 
 app :: Maybe Connection -> Maybe Mysql.MySQLConn -> IO Application
@@ -24,19 +25,26 @@ app disqueConnection mysqlConnection =
     let subscriber = Subscriber 1 "" "" ""
     middleware logStdout -- log all requests; for production use logStdout
     get "/subscribers" . json $ subscriber
-    post "/subscribers" addSubscriber
+    post "/subscribers" (addSubscriber mysqlConnection)
     get "/tasks" . json $ task
     post "/tasks/:queue/:value/:expire" (shouldAddTask disqueConnection)
 
 shouldAddTask :: Maybe Connection -> ActionM ()
 shouldAddTask (Just connection) = addTask connection
-shouldAddTask Nothing           = text "disque current offline!"
+shouldAddTask Nothing = do
+      text "disque current offline!"
+      status status500
 
-addSubscriber :: ActionM ()
-addSubscriber = do
+addSubscriber :: Maybe Mysql.MySQLConn -> ActionM ()
+addSubscriber (Just connection) = do
           subscriber <- parseSubscriber
+          insertSubscriber connection subscriber
           json $ subscriber
           status created201
+addSubscriber Nothing = do
+        text "mysql current offline!"
+        status status500
+
 
 
 -- Parse the request body into the Subscriber
---
+++ src/Database/Subscriber.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Database.Subscriber where
+
+import Control.Monad.IO.Class
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.Text as T
+import Web.Scotty.Internal.Types (ActionT)
+import Database.MySQL.Base
+import Types.Subscriber
+import System.IO.Streams
+import System.IO.Streams.List
+import Data.String.Conversions              (cs)
+
+
+usersTableSQL :: Query
+usersTableSQL =
+    "CREATE TABLE IF NOT EXISTS subscriber (id BIGINT NOT NULL AUTO_INCREMENT, target VARCHAR(255) NOT NULL,code VARCHAR(255) NOT NULL, userid VARCHAR(255) NOT NULL,CONSTRAINT pk PRIMARY KEY (lid));"
+
+insertTokenSQL :: Query
+insertTokenSQL =
+    "INSERT INTO subscriber (target, code, userid) VALUES (?, ?, ?);"
+
+
+insertSubscriber :: MySQLConn -> Maybe Subscriber -> ActionT TL.Text IO ()
+insertSubscriber mysqlConnection Nothing = return ()
+insertSubscriber mysqlConnection (Just (Subscriber id_ target_ code_ userid_)) = do
+  _ <- liftIO $ execute mysqlConnection insertTokenSQL [MySQLText (cs target_), MySQLText (cs code_), MySQLText (cs userid_)]
+  return ()
+
+
+drain :: IO ([ColumnDef], InputStream [MySQLValue]) -> IO [[MySQLValue]]
+drain action = do
+    (_,ist) <- action
+    System.IO.Streams.List.toList ist

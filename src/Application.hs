072347625f3d1648f282d2f8a9fc45de88e7d3a1
Arek Czarnik <arekczarnik@gmail.com>
2017-06-27 22:29:23 +0200

    make select now working.

--- src/Application.hs
+++ src/Application.hs
@@ -2,29 +2,28 @@
 
 module Application where
 
-import           Data.Aeson                           (encode,decode)
-import           Data.String.Conversions              (cs)
-import           Database.Disque
+import Control.Monad.IO.Class
+import Data.Aeson (decode, encode)
+import Data.String.Conversions (cs)
+import Data.Text.Lazy (Text)
+import Database.Disque
 import qualified Database.MySQL.Base as Mysql
-import           Handlers.Task
-import           Network.Wai                          (Application)
-import           Network.Wai.Middleware.RequestLogger (logStdout)
-import           Types.Subscriber
-import           Types.Task
-import           Web.Scotty
-import           Web.Scotty.Internal.Types (ActionT)
-import           Network.HTTP.Types.Status
-import           Data.Text.Lazy (Text)
-import           Database.Subscriber
-
+import Database.Subscriber
+import Handlers.Task
+import Network.HTTP.Types.Status
+import Network.Wai (Application)
+import Network.Wai.Middleware.RequestLogger (logStdout)
+import Types.Subscriber
+import Types.Task
+import Web.Scotty
+import Web.Scotty.Internal.Types (ActionT)
 
 app :: Maybe Connection -> Maybe Mysql.MySQLConn -> IO Application
 app disqueConnection mysqlConnection =
   scottyApp $ do
     let task = Task 1 "1" "1" 0
-    let subscriber = Subscriber 1 "" "" ""
     middleware logStdout -- log all requests; for production use logStdout
-    get "/subscribers" . json $ subscriber
+    get "/subscribers" $ showSubscribers mysqlConnection
     post "/subscribers" (addSubscriber mysqlConnection)
     get "/tasks" . json $ task
     post "/tasks/:queue/:value/:expire" (shouldAddTask disqueConnection)
@@ -32,20 +31,26 @@ app disqueConnection mysqlConnection =
 shouldAddTask :: Maybe Connection -> ActionM ()
 shouldAddTask (Just connection) = addTask connection
 shouldAddTask Nothing = do
-      text "disque current offline!"
-      status status500
+  text "disque current offline!"
+  status status500
+
+showSubscribers :: Maybe Mysql.MySQLConn -> ActionM ()
+showSubscribers (Just connection) = do
+  subscribers <- liftIO $ listSubscriber connection
+  json subscribers
+showSubscribers Nothing = do
+  text "mysql current offline!"
+  status status500
 
 addSubscriber :: Maybe Mysql.MySQLConn -> ActionM ()
 addSubscriber (Just connection) = do
-          subscriber <- parseSubscriber
-          insertSubscriber connection subscriber
-          json $ subscriber
-          status created201
+  subscriber <- parseSubscriber
+  insertSubscriber connection subscriber
+  json subscriber
+  status created201
 addSubscriber Nothing = do
-        text "mysql current offline!"
-        status status500
-
-
+  text "mysql current offline!"
+  status status500
 
 -- Parse the request body into the Subscriber
 parseSubscriber :: ActionT Text IO (Maybe Subscriber)
